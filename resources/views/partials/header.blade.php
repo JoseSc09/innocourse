07eff1b3a9a74260c9d1ddaf35d338063b770f4d
jose.scalderon96 <jose.scalderon96@gmail.com>
2024-09-11 18:34:09 +0000

    Header Responsive

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,31 +1,35 @@
-
-    <nav class="bg-slate-800 py-4 md:py-2 text-gray-200 flex flex-col md:flex-row items-center gap-6 md:justify-around text-xl font-semibold">
-        <div>
-            <img class="rounded-full w-24" src="{{ Vite::image('Logo.jpg') }}" alt="Logo Inno Course">
+<nav class="bg-slate-800 py-4 px-4 md:px-8 text-gray-200 flex flex-col md:flex-row items-center gap-6 md:justify-between text-lg md:text-xl font-semibold">
+        <div class="flex justify-between w-full md:w-auto items-center">
+            <img class="rounded-full w-16 md:w-24" src="{{ Vite::image('Logo.jpg') }}" alt="Logo Inno Course">
+            <!-- Botón del menú para móviles -->
+            <button id="menu-toggle" class="md:hidden text-gray-200 focus:outline-none">
+                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
         </div>
-        <div class="">
-            <ul class="flex flex-col md:flex-row gap-[4vw]">
+    
+        <!-- Menú de navegación -->
+        <div id="nav-menu" class="hidden md:flex flex-col md:flex-row items-center w-full md:w-auto">
+            <ul class="flex flex-col md:flex-row gap-6 md:gap-8 w-full md:w-auto text-center md:text-left">
                 <li>
-                    <a class="hover:text-white" href="{{ route('home.index') }}">Inicio</a>
+                    <a class="hover:text-white transition-colors duration-300 text-center" href="{{ route('home.index') }}">Inicio</a>
                 </li>
                 <li>
-                    <a class="hover:text-white" href="{{ route('courses.index') }}">Cursos</a>
+                    <a class="hover:text-white transition-colors duration-300 text-center" href="{{ route('courses.index') }}">Cursos</a>
                 </li>
                 <li>
-                    <a class="hover:text-white" href="{{ route('about.index') }}">Sobre Nosotros</a>
+                    <a class="hover:text-white transition-colors duration-300 text-center" href="{{ route('about.index') }}">Sobre Nosotros</a>
                 </li>
                 <li>
-                    <a class="hover:text-white" href="{{ route('opinions.index') }}">Opinión</a>
+                    <a class="hover:text-white transition-colors duration-300 text-center" href="{{ route('opinions.index') }}">Opinión</a>
                 </li>
             </ul>
         </div>
-        <div>
-            <input class="rounded-full p-2" type="text" placeholder="Buscar...">
-        </div>
-        <div>
-            <button id="night-mode-toggle">🌓</button>
-        </div>
-        <div>
-            <button class="rounded-full text-white bg-purple-500 px-5 py-2 hover:bg-purple-600" type="button" href="{{ route('login.index') }}">Iniciar Sesión</button>
+    
+        <div class="flex flex-col md:flex-row items-center gap-4 md:gap-6 w-full md:w-auto">
+            <input class="rounded-full p-2 w-full md:w-48 text-black focus:outline-none" type="text" placeholder="Buscar...">
+            <button id="night-mode-toggle" class="md:ml-4">🌓</button>
+            <button class="rounded-full text-white bg-purple-500 px-5 py-2 hover:bg-purple-600 transition-colors duration-300 w-full md:w-auto" type="button" href="{{ route('login.index') }}">Iniciar Sesión</button>
         </div>
     </nav>
