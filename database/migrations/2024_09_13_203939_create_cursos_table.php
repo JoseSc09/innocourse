12cf20c4596d75e8d07dc4adb81000880b571ac5
JoseSc <jose.scalderon96@gmail.com>
2024-09-14 05:18:45 -0500

    agregando el campo imagen a la tabla cursos

--- database/migrations/2024_09_13_203939_create_cursos_table.php
+++ database/migrations/2024_09_13_203939_create_cursos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id('curso_id');
+            $table->string('imagen')->nullable();
             $table->string('titulo');
             $table->text('descripcion')->nullable();
             $table->decimal('precio', 10, 2);
