ee48c2a0d7312ab23186fb83a6b79243a736a6db
Max Yarasca <yarasca.wd@gmail.com>
2024-09-14 04:13:09 -0500

    Configurando Vite para la carpeta Storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::macro('image', fn (string $asset) => $this->asset("resources/images/{$asset}"));
+        Vite::macro('image', function (string $asset) {
+            // Si el archivo está en 'public/storage/imagenes', devuelve la URL pública
+            if (Storage::disk('public')->exists($asset)) {
+                return asset('storage/' . $asset);
+            }
+        
+            // Si no está en 'storage', busca en 'resources/images' y usa Vite para manejarlo
+            return $this->asset("resources/images/{$asset}");
+        });
     }
 }
