8050686fa04709565907ab4f9f1eed368fa3a6c9
Max Yarasca <yarasca.wd@gmail.com>
2024-09-14 04:28:38 -0500

    Configurando Vite para las imagenes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,13 +23,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::macro('image', function (string $asset) {
-            // Si el archivo está en 'public/storage/imagenes', devuelve la URL pública
+            // Si el archivo está en 'public/storage', devuelve la URL pública
             if (Storage::disk('public')->exists($asset)) {
-                return asset('storage/' . $asset);
+                return URL::to('storage/' . $asset);
             }
         
-            // Si no está en 'storage', busca en 'resources/images' y usa Vite para manejarlo
-            return $this->asset("resources/images/{$asset}");
+            // Si el archivo no está en 'storage', se considera que está en 'resources/images'
+            // Vite normalmente maneja archivos en 'public', no directamente en 'resources'
+            // Asegúrate de que los archivos están en 'public' o que están bien manejados por Vite
+            return Vite::asset("resources/images/{$asset}");
         });
     }
 }
