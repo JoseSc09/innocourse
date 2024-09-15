4a481fe989e6d976a216a7d273c0b68c88200a49
JoseSc <jose.scalderon96@gmail.com>
2024-09-15 17:39:00 -0500

    Actualizando los seeders

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -15,19 +15,49 @@ class UsuarioSeeder extends Seeder
     {
         //
         $usuario = new Usuario();
-        $usuario->nombre = 'Admin';
-        $usuario->apellido = 'Admin';
-        $usuario->email = 'admin@mail.com';
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Jose';
+        $usuario->apellido = 'Calderon';
+        $usuario->email = 'jose.calderon@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 1;
+        $usuario->save();
+
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/instructor_carla_casas.jpg';
+        $usuario->nombre = 'Carla';
+        $usuario->apellido = 'Casas';
+        $usuario->email = 'carla.c@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 2;
         $usuario->save();
 
         $usuario = new Usuario();
-        $usuario->nombre = 'Juanito';
-        $usuario->apellido = 'PicaPiedra';
-        $usuario->email = 'juan@mail.com';
+        $usuario->imagen = 'storage/images/usuarios/instructor_andres_galicia.jpg';
+        $usuario->nombre = 'Andres';
+        $usuario->apellido = 'Galicia';
+        $usuario->email = 'andres.g@mail.com';
         $usuario->password = bcrypt('123456789');
-        $usuario->rol_id= 3;
+        $usuario->rol_id= 2;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/instructor_luz_hernandez.jpg';
+        $usuario->nombre = 'Luz';
+        $usuario->apellido = 'Hernandez';
+        $usuario->email = 'luz.h@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 2;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/instructor_juan_maya.jpg';
+        $usuario->nombre = 'Juan';
+        $usuario->apellido = 'Maya';
+        $usuario->email = 'juan.m@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 2;
         $usuario->save();
     }
 }
