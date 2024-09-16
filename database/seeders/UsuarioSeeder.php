e377db39943d8a0de21ffd0803da40225e7d03e6
JoseSc <jose.scalderon96@gmail.com>
2024-09-16 00:24:35 -0500

    configurando seeders

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -18,6 +18,7 @@ class UsuarioSeeder extends Seeder
         $usuario->imagen = 'storage/images/usuarios/user_default.png';
         $usuario->nombre = 'Jose';
         $usuario->apellido = 'Calderon';
+        $usuario->username = 'admin';
         $usuario->email = 'jose.calderon@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 1;
@@ -28,6 +29,7 @@ class UsuarioSeeder extends Seeder
         $usuario->imagen = 'storage/images/usuarios/instructor_carla_casas.jpg';
         $usuario->nombre = 'Carla';
         $usuario->apellido = 'Casas';
+        $usuario->username = 'Carla_CasasH';
         $usuario->email = 'carla.c@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 2;
@@ -37,6 +39,7 @@ class UsuarioSeeder extends Seeder
         $usuario->imagen = 'storage/images/usuarios/instructor_andres_galicia.jpg';
         $usuario->nombre = 'Andres';
         $usuario->apellido = 'Galicia';
+        $usuario->username = 'Andres_GaliciaC';
         $usuario->email = 'andres.g@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 2;
@@ -46,6 +49,7 @@ class UsuarioSeeder extends Seeder
         $usuario->imagen = 'storage/images/usuarios/instructor_luz_hernandez.jpg';
         $usuario->nombre = 'Luz';
         $usuario->apellido = 'Hernandez';
+        $usuario->username = 'Luz_HernandezH';
         $usuario->email = 'luz.h@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 2;
@@ -55,9 +59,70 @@ class UsuarioSeeder extends Seeder
         $usuario->imagen = 'storage/images/usuarios/instructor_juan_maya.jpg';
         $usuario->nombre = 'Juan';
         $usuario->apellido = 'Maya';
+        $usuario->username = 'Juan_MayaR';
         $usuario->email = 'juan.m@mail.com';
         $usuario->password = bcrypt('123456789');
         $usuario->rol_id= 2;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Carlos';
+        $usuario->apellido = 'Gomez';
+        $usuario->username = 'Carlos_Gomez15';
+        $usuario->email = 'carlos.g@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Maria';
+        $usuario->apellido = 'Jimenez';
+        $usuario->username = 'J.Maria200';
+        $usuario->email = 'maria.j@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Elena';
+        $usuario->apellido = 'Peralta';
+        $usuario->username = 'E.Peralta78';
+        $usuario->email = 'elena.p@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Luis';
+        $usuario->apellido = 'Fernandez';
+        $usuario->username = 'Luis.Fndz69';
+        $usuario->email = 'luis.f@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Ricardo';
+        $usuario->apellido = 'Tavares';
+        $usuario->username = 'Ricardo.Tvrs35';
+        $usuario->email = 'ricardo.t@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->imagen = 'storage/images/usuarios/user_default.png';
+        $usuario->nombre = 'Mario';
+        $usuario->apellido = 'Casas';
+        $usuario->username = 'M.Casas45';
+        $usuario->email = 'mario.c@mail.com';
+        $usuario->password = bcrypt('123456789');
+        $usuario->rol_id= 3;
         $usuario->save();
     }
 }
