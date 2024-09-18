c74ec8529ec0f2428bd06fa250da4f6cae2d39b0
JoseSc <jose.scalderon96@gmail.com>
2024-09-18 15:14:55 -0500

    dark mode fixed

--- resources/views/pages/sobre-nosotros/index.blade.php
+++ resources/views/pages/sobre-nosotros/index.blade.php
@@ -7,8 +7,10 @@
                         alt="Imagen de header">
                 </div>
                 <div class="lg:col-span-2 flex flex-col justify-around">
-                    <h1 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-end">{{ $principal->titulo }}</h1>
-                    <p class="lg:text-xl text-center md:text-end md:leading-relaxed lg:leading-loose">{{ $principal->texto }}</p>
+                    <h1 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-end">
+                        {{ $principal->titulo }}</h1>
+                    <p class="lg:text-xl text-center md:text-end md:leading-relaxed lg:leading-loose">
+                        {{ $principal->texto }}</p>
                 </div>
             </div>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-6 md:mb-12">
@@ -17,8 +19,10 @@
                         alt="Imagen de misión">
                 </div>
                 <div class="lg:col-span-2 flex flex-col justify-around md:col-start-1">
-                    <h2 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-start">{{ $mision->titulo }}</h2>
-                    <p class="lg:text-xl text-center md:text-start md:leading-relaxed lg:leading-loose">{{ $mision->texto }}</p>
+                    <h2 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-start">
+                        {{ $mision->titulo }}</h2>
+                    <p class="lg:text-xl text-center md:text-start md:leading-relaxed lg:leading-loose">
+                        {{ $mision->texto }}</p>
                 </div>
             </div>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-6 md:mb-12">
@@ -27,8 +31,10 @@
                         alt="Imagen de visión">
                 </div>
                 <div class="lg:col-span-2 flex flex-col justify-around">
-                    <h2 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-end">{{ $vision->titulo }}</h2>
-                    <p class="lg:text-xl text-center md:text-end md:leading-relaxed lg:leading-loose">{{ $vision->texto }}</p>
+                    <h2 class="font-sans font-bold text-xl lg:text-3xl uppercase text-center md:text-end">
+                        {{ $vision->titulo }}</h2>
+                    <p class="lg:text-xl text-center md:text-end md:leading-relaxed lg:leading-loose">
+                        {{ $vision->texto }}</p>
                 </div>
             </div>
             <div class="my-28">
@@ -44,7 +50,7 @@
                 <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     @foreach ($cursos as $curso)
                         <div
-                            class="max-w-sm bg-gray-50 border border-gray-300 rounded-lg shadow-lg dark:border dark:border-gray-900 dark:bg-gray-800">
+                            class="max-w-sm bg-gray-50 border border-gray-300 rounded-lg shadow-lg dark:border-none dark:border-gray-900 dark:bg-indigo-950">
                             <img class="rounded-t-lg" src="{{ asset($curso->instructor->imagen) }}" alt="" />
                             <div class="p-5">
                                 <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -53,7 +59,7 @@
                                     class="text-sm text-gray-600 tracking-tight dark:text-gray-400">{{ $curso->nombre }}</span>
                                 <p class="my-3 font-normal text-gray-700 dark:text-gray-400">Curso {{ $curso->titulo }}
                                 </p>
-                                <a href="#"
+                                <a href="{{route('courses.index')}}"
                                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Ver cursos
                                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
@@ -68,7 +74,7 @@
                 </div>
             </div>
         </div>
-        
+
         {{-- Circulos --}}
         <div class="circle-first bg-purple-400 dark:bg-purple-500"></div>
         <div class="circle-second bg-purple-400 dark:bg-purple-500"></div>
