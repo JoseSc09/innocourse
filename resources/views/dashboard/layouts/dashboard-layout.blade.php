8928afe4332111bc22a592544ef641fde44afc3e
JoseSc <jose.scalderon96@gmail.com>
2024-09-19 03:02:58 -0500

    update dashboard

--- resources/views/dashboard/layouts/dashboard-layout.blade.php
+++ resources/views/dashboard/layouts/dashboard-layout.blade.php
@@ -5,14 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Admin</title>
+    <title> Admin </title>
 
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 
-<body class="dark:bg-slate-800">
-    {{ $slot }}
+<body class="bg-gray-100">
+
+    <!-- Contenedor Principal -->
+    <div class="flex h-screen">
+
+        <x-side-bar>
+        </x-side-bar>
+
+        {{ $slot }}
+    </div>
 </body>
 
 </html>
