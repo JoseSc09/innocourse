78cd7099e9c013cc0b16f716fcbe4478da3fb87e
Max Yarasca <yarasca.wd@gmail.com>
2024-09-21 04:39:06 -0500

    Separando admin.js del app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const applyDarkMode = (isDarkMode) => {
     if (isDarkMode) {
       html.classList.add('dark'); // Cambia a html
+      darkModeToggle.classList.add('active');
     } else {
       html.classList.remove('dark'); // Cambia a html
+      darkModeToggle.classList.remove('active');
+
     }
   };
 
@@ -99,40 +102,4 @@ document.addEventListener("DOMContentLoaded", function () {
     navMenu.classList.toggle("hidden");
     navMenu.classList.toggle("flex");
   });
-});
-
-// Script para mantener el foco activo en el módulo seleccionado
-const links = document.querySelectorAll('aside nav ul li a');
-
-links.forEach(link => {
-  link.addEventListener('click', function (event) {
-    // Elimina el estilo activo de todos los enlaces
-    links.forEach(l => l.classList.remove('bg-gray-700'));
-
-    // Agrega el estilo activo solo al enlace seleccionado
-    link.classList.add('bg-gray-700');
-  });
-});
-
-// Script para el menú hamburguesa
-const menuToggle = document.getElementById('menuToggle');
-const sidebar = document.getElementById('sidebar');
-const sidebarText = document.querySelectorAll('.sidebar-text');
-const sidebarTitle = document.getElementById('sidebarTitle');
-
-menuToggle.addEventListener('click', () => {
-  // Alterna entre mostrar/ocultar los textos de los elementos del sidebar
-  sidebar.classList.toggle('w-64'); // Cambia el ancho del sidebar
-  sidebarText.forEach(text => text.classList.toggle('hidden')); // Muestra/Oculta los textos
-  sidebarTitle.classList.toggle('hidden'); // Muestra/Oculta el título del sidebar
-
-  // Actualiza el estado aria-expanded
-  const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
-  menuToggle.setAttribute('aria-expanded', !isExpanded);
-});
-
-//Sidebar
-document.querySelector("[data-collapse-toggle='dropdown-users']").addEventListener("click", function () {
-  const dropdown = document.getElementById("dropdown-users");
-  dropdown.classList.toggle("hidden");
 });
