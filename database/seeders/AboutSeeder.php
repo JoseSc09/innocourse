056b490b15f21d6b0e08b27a036d98aa6610b68e
JoseSc <jose.scalderon96@gmail.com>
2024-09-25 14:04:34 -0500

    update seeders

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -15,22 +15,22 @@ class AboutSeeder extends Seeder
     {
         $about = new About();
         $about->id_about = 1;
-        $about->titulo = 'Innocourse forma profesionales para la vida tecnológica';
-        $about->texto = 'Somos InnoCourse, una plataforma educativa dedicada a fusionar innovación y aprendizaje. Nuestro objetivo es proporcionar experiencias de aprendizaje transformadoras que impulsen el desarrollo personal y profesional. Nos especializamos en crear cursos dinámicos y actualizados que preparan a nuestros estudiantes para los desafíos del mundo moderno.';
+        $about->titulo = 'innocourse forma profesionales para la vida tecnológica';
+        $about->texto = 'somos InnoCourse, una plataforma educativa dedicada a fusionar innovación y aprendizaje. Nuestro objetivo es proporcionar experiencias de aprendizaje transformadoras que impulsen el desarrollo personal y profesional. Nos especializamos en crear cursos dinámicos y actualizados que preparan a nuestros estudiantes para los desafíos del mundo moderno.';
         $about->imagen = 'storage/images/about/about_img1.jpg';
         $about->save();
 
         $about = new About();
         $about->id_about = 2;
-        $about->titulo = 'MISIÓN';
-        $about->texto = 'Empoderar a los estudiantes mediante herramientas educativas innovadoras y accesibles que fomenten el aprendizaje continuo y el desarrollo de habilidades críticas para el futuro. Nos comprometemos a ofrecer una educación de alta calidad, adaptada a las necesidades cambiantes del mercado global y centrada en el éxito de nuestros estudiantes.';
+        $about->titulo = 'misión';
+        $about->texto = 'empoderar a los estudiantes mediante herramientas educativas innovadoras y accesibles que fomenten el aprendizaje continuo y el desarrollo de habilidades críticas para el futuro. Nos comprometemos a ofrecer una educación de alta calidad, adaptada a las necesidades cambiantes del mercado global y centrada en el éxito de nuestros estudiantes.';
         $about->imagen = 'storage/images/about/about_mision.jpg';
         $about->save();
 
         $about = new About();
         $about->id_about = 3;
-        $about->titulo = 'VISIÓN';
-        $about->texto = 'Ser la plataforma líder en educación innovadora, reconocida por transformar la manera en que las personas aprenden y aplican conocimientos, creando un impacto positivo en sus vidas y en la sociedad.';
+        $about->titulo = 'visión';
+        $about->texto = 'ser la plataforma líder en educación innovadora, reconocida por transformar la manera en que las personas aprenden y aplican conocimientos, creando un impacto positivo en sus vidas y en la sociedad.';
         $about->imagen = 'storage/images/about/about_vision.jpg';
         $about->save();
     }
