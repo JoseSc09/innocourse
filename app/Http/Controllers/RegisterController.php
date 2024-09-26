b2bee300ecb5177e4d273def270d25e76816c733
JoseSc <jose.scalderon96@gmail.com>
2024-09-26 04:00:14 -0500

    update registerController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,17 +16,17 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         $validatedData = $request->validate([
-            'nombre' => 'required|string|max:255',
-            'apellido' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'username' => 'required|string|max:255|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
         //Asignar imagen por defecto
-        $validatedData['imagen'] = $request->input('imagen','storage/images/usuarios/user_default.png');
+        $validatedData['image'] = $request->input('image','storage/images/users/user_default.png');
 
-        // Asignar el rol de estudiante por defecto a estudiante
+        // Asignar el rol de estudiante por defecto
         $validatedData['rol_id'] = $request->input('rol_id', 3);
 
         // Crear el usuario
