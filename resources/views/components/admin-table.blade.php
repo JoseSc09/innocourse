ff0d22ec4bfb33f388b5acd5c4f14da43bf1e423
JoseSc <jose.scalderon96@gmail.com>
2024-09-30 10:02:18 -0500

    update admin table component

--- resources/views/components/admin-table.blade.php
+++ resources/views/components/admin-table.blade.php
@@ -14,16 +14,22 @@
                 <tr
                     class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                     @foreach ($row as $cell)
-                        <td class="px-6 py-4 {{ isset($cell['delete_link']) != null ? 'flex gap-6' : '' }}">
-                            <p class=" line-clamp-4">
+                        <td class="px-6 py-4">
+                            <p class=" line-clamp-4 {{ isset($cell['delete_link']) != null ? 'flex gap-6' : '' }}">
 
                                 @if (isset($cell['avatar']))
                                     <img class="w-12 h-12 rounded-full object-cover" src="{{ $cell['avatar'] }}"
                                         alt="Avatar">
                                 @elseif (isset($cell['image']))
-                                    <img class="min-w-48 h-36 rounded-md object-cover" src="{{ $cell['image'] }}"
+                                    <img class="min-w-48 h-36 object-scale-down" src="{{ $cell['image'] }}"
                                         alt="Imagen">
-                                @elseif (isset($cell['edit_link']) || isset($cell['delete_link']))
+                                @elseif (isset($cell['edit_link']) || isset($cell['delete_link']) || isset($cell['show_link']))
+                                    @if (isset($cell['show_link']))
+                                        <a href="{{ $cell['show_link'] }}"
+                                            class="font-medium text-blue-500 dark:text-blue-400 text-2xl">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    @endif
                                     @if (isset($cell['edit_link']))
                                         <a href="{{ $cell['edit_link'] }}"
                                             class="font-medium text-green-600 dark:text-green-500 text-2xl">
@@ -31,7 +37,7 @@
                                         </a>
                                     @endif
                                     @if (isset($cell['delete_link']))
-                                        <button onclick="openModal({{ $cell['id'] }})"
+                                        <button onclick="openModal('{{ $cell['routeDelete'] }}')"
                                             class="font-medium text-red-600 dark:text-red-500 text-2xl">
                                             <i class="fas fa-trash"></i>
                                         </button>
@@ -47,4 +53,3 @@
         </tbody>
     </table>
 </div>
-
