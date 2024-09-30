41f736564530b7d621fc2ae8087acaa796c80cd9
JoseSc <jose.scalderon96@gmail.com>
2024-09-30 10:00:15 -0500

    update course page controller

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Course;
-
+use App\Models\Subscription;
 
 class CoursesController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $courses = Course::withAvg('reviews', 'rating')->paginate(8);
         $categories = Category::all();
-        return view('pages.cursos.index',compact('courses','categories'));
+        return view('pages.cursos.index', compact('courses', 'categories'));
     }
 
-    public function show($slug){
-        $course = Course::where('slug',$slug)->withAvg('reviews', 'rating')->first(); 
-        return view('pages.cursos.show',compact('course'));
+    public function show($slug)
+    {
+        $course = Course::where('slug', $slug)->with('sections.lessons')->withAvg('reviews', 'rating')->firstOrFail();
+        $subscriptions = Subscription::all();
+        return view('pages.cursos.show', compact('course','subscriptions'));
     }
 }
